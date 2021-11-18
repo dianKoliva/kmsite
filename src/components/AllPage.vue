46f1d42466edabd8ddf0b0960920d54288cfe283
dianKoliva <dianpretty01@gmail.com>
2021-11-18 16:54:20 +0200

    proj setup

---
+++ src/components/AllPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="holder">
+    <div class="logo"></div>
+    <div class="nav relative right-0">
+      <Nav></Nav>
+    </div>
+  </div>
+</template>
+
+<script>
+import Nav from "./Nav.vue";
+
+export default {
+  name: "ALl",
+  components: { Nav },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="holder">hey</div>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+};
+</script>
+
+<style scoped>
+</style>
