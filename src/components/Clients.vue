efcd5de24938b3a862a9028ee8d1c9036ac266b2
dianKoliva <dianpretty01@gmail.com>
2021-11-21 21:11:30 +0200

    new changes

--- src/components/Clients.vue
+++ src/components/Clients.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="w-full bg-red-light">
-    <p class="font-bold">Our Clients Speak</p>
+    <p class="font-bold text-xl text-red">Our Clients Speak</p>
+
+    <div class="flex">
+      <div class="w-72 ml-20 mt-10 text-sm">
+        <img src="/d.png" class="w-10" alt="" />
+        <p>Diane Pretty</p>
+        <span class="">
+          “Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum “
+        </span>
+      </div>
+      <div class="w-72 ml-20 mt-10 text-sm">
+        <img src="/d.png" class="w-10" alt="" />
+        <p>Diane Pretty</p>
+        <span class="">
+          “Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum “
+        </span>
+      </div>
+      <div class="w-72 ml-20 mt-10 text-sm">
+        <img src="/d.png" class="w-10" alt="" />
+        <p>Diane Pretty</p>
+        <span class="">
+          “Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum “
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/components/Services.vue
+++ src/components/Services.vue
@@ -2,10 +2,10 @@
   <div class="flex pb-10 mt-16">
     <div class="w-1/2 mt-32">
       <div class="ml-24 w-96">
-        <h1 class="text-xl text-red font-bold mt-10">
+        <h1 class="text-xl text-red font-extrabold mt-10">
           SERVICES WE CAN HELP YOU WITH
         </h1>
-        <p class="text-sm">
+        <p class="text-sm leading-7 mt-4">
           The services listed on the right side are the services you can find at
           Koliva motors.Come to Koliva Motors receive the best services of all
           time.
@@ -15,8 +15,8 @@
     <div class="w-1/2 flex">
       <div class="mt-12 ml-8">
         <div class="bg-red-lighter w-40 text-center rounded-lg shadow-lg">
-          <i class="fa fa-search text-red text-3xl pt-4"></i>
-          <p class="text-sm pl-2 pb-4 pr-2 pt-4">
+          <i class="fa fa-truck text-red text-2xl pt-4"></i>
+          <p class="text-sm pl-2 pb-4 pr-2 pt-2">
             Koliva Motors can give your truck a new look
           </p>
         </div>
@@ -32,8 +32,8 @@
             ml-6
           "
         >
-          <i class="fa fa-search text-red text-3xl pt-4"></i>
-          <p class="text-sm pl-2 pb-4 pr-2 pt-4">
+          <i class="fa fa-money text-red text-2xl pt-4"></i>
+          <p class="text-sm pl-2 pb-4 pr-2 pt-2">
             Koliva Motors sells the best used trucks in the city
           </p>
         </div>
@@ -41,8 +41,8 @@
 
       <div class="ml-14">
         <div class="bg-red-lighter w-40 text-center rounded-lg shadow-lg">
-          <i class="fa fa-search text-red text-3xl pt-4"></i>
-          <p class="text-sm pl-2 pb-4 pr-2 pt-4">
+          <i class="fa fa-money text-red text-2xl pt-4"></i>
+          <p class="text-sm pl-2 pb-4 pr-2 pt-2">
             You can also sell your truck to Koliva Motors
           </p>
         </div>
@@ -58,9 +58,9 @@
             ml-6
           "
         >
-          <i class="fa fa-search text-red text-3xl pt-4"></i>
-          <p class="text-sm pl-2 pb-4 pr-2 pt-4">
-            Koliva Motors can give your track a new look
+          <i class="fa fa-wrench text-red text-2xl pt-4"></i>
+          <p class="text-sm pl-2 pb-4 pr-2 pt-2">
+            Koliva Motors sends you a mechanic to repair your truck.
           </p>
         </div>
       </div>
