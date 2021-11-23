c8c15775812d8ee51329d7f7ce3f0fc4fd97b411
dianKoliva <dianpretty01@gmail.com>
2021-11-23 19:17:35 +0200

    new changes

--- src/components/Clients.vue
+++ src/components/Clients.vue
@@ -6,38 +6,54 @@
     <div>
       <div v-if="show == 1" class="flex mt-10 pb-6">
         <div class="w-72 ml-20 mt-10 text-sm">
-          <img src="/d.png" class="w-10 ml-auto mr-auto" alt="" />
-          <p class="text-center pt-1 pb-2">Diane Pretty</p>
-          <span class="">
-            “kkLorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum “
+          <img
+            src="/three.jpg"
+            class="ml-auto mr-auto rounded-full h-12 w-12"
+            alt=""
+          />
+          <p class="text-center pt-1 pb-2">Akaliza Ange</p>
+          <span class="text-center">
+            “I went to Koliva Motors when my truck was damaged and I was
+            suprised by the hospitality and good service at the worksop “
           </span>
         </div>
         <div class="w-72 ml-24 mt-10 text-sm">
-          <img src="/d.png" class="w-10 ml-auto mr-auto" alt="" />
-          <p class="text-center pt-1 pb-2">Diane Pretty</p>
-          <span class="">
-            “Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum “
+          <img
+            src="/four.jpg"
+            class="w-12 h-12 ml-auto mr-auto rounded-full"
+            alt=""
+          />
+          <p class="text-center pt-1 pb-2 text-sm">Keza Divine</p>
+          <span class="text-center">
+            “I can testify that Koliva motors is the best chassis repairing
+            company in Rwanda after renewing my truck “
           </span>
         </div>
         <div class="w-72 ml-24 mt-10 text-sm">
-          <img src="/d.png" class="w-10 ml-auto mr-auto" alt="" />
-          <p class="text-center pt-1 pb-2">Diane Pretty</p>
+          <img
+            src="/11.jpg"
+            class="w-12 h-12 rounded-full ml-auto mr-auto"
+            alt=""
+          />
+          <p class="text-center pt-1 pb-2">Kizito Flex</p>
           <span class="">
-            “Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum “
+            “I was searching for the best place to sell my truck , they
+            recommended me Koliva motors and they sold it at a good price “
           </span>
         </div>
       </div>
 
       <div v-else class="flex mt-10 pb-6">
         <div class="w-72 ml-20 mt-10 text-sm">
-          <img src="/d.png" class="w-10 ml-auto mr-auto" alt="" />
-          <p class="text-center pt-1 pb-2">Diane Pretty</p>
+          <img
+            src="/12.jpg"
+            class="w-12 h-12 ml-auto mr-auto rounded-full"
+            alt=""
+          />
+          <p class="text-center pt-1 pb-2">Ngenzi David</p>
           <span class="">
-            “Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum “
+            “If you are looking for the best garage to get your car chassis
+            repaired, think of Koliva Motors“
           </span>
         </div>
         <div class="w-72 ml-24 mt-10 text-sm">
